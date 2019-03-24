99f288196f94b5614def1ca58d482f1eedd9c28b
Shashank Kuchibhotla <skuchibhotla@outlook.com>
2019-03-24 02:56:50 -0400

    Initial commit

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+        
+        public ApplicationUser Artist { get; set; }
+
+        [Required]
+        public string ArtistId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+    }
+}
